4d2eb81b050d1bfea059dd4623770107fcc3fc2e
Luciano Moura de Melo Júnior <lucianomoura.dev@gmail.com>
2026-03-14 22:00:10 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
-console.log('Mão na massa!');
+console.log('Mão na massa!')
 
-const offset = 0;
-const limit = 20;
+const offset = 0
+const limit = 20
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+//Promisses são objetos que representam o resultado de uma operação assíncrona. Elas podem estar em um dos seguintes estados:
+
 fetch(url)
-    .then(function(response) {
-        console.log(response);
-    })
-    .catch(function(error) {
-        console.error(error);
-    })
+    .then ((response) => response.json())
+    .then ((jsonBody) => console.log(jsonBody))
+
+    .catch((error) => console.error(error))
+
     .finally(function() {
-        console.log('Requisição finalizada!');
+        console.log('Requisição finalizada!')
     })
