3731c12ba8abad4487daf9b20100a3cbcf06479a
Luciano Moura de Melo Júnior <lucianomoura.dev@gmail.com>
2026-03-17 14:18:39 -0300

    Updade all

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,37 +7,33 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 
 function convertPokemonToLi(pokemon) {
-    return`
-        <li class="pokemon" id="grass">
-            <div class="pokemon-header">
+    return `
+        <li class="pokemon" id="pokemon">
+           <div class="pokemon-header">
                 <span class="pokemon-name">${pokemon.name}</span>
-                <span class="pokemon-number">#${pokemon.number}</span>
+                <span class="pokemon-number">#001</span>
             </div>
             <div class="pokemon-info">
                 <ol class="type">
-                    <li class="type_grass">Grass</li>
-                    <li class="type_poison">Poison</li>
+                    <li class="race">Grass</li>
+                    <li class="attack">Poison</li>
                 </ol>
-                <img src="https://www.smogon.com/dex/media/sprites/xy/${pokemon.name}.gif" alt="${pokemon.name}">
-            </div>
+                <img src="https://www.smogon.com/dex/media/sprites/xy/${pokemon.name.toLowerCase()}.gif" alt="${pokemon.name}">
+           </div>
         </li>
     `
 }
 
-const pokemonList = document.getElementById('pokemonList')
+const pokemonList = document.getElementById('pokemonList');
 
 fetch(url)
     .then ((response) => response.json())
-    .then ((jsonBody) => jasonBody.results)
-    .then ((pokemonList) => {
-        for (let i = 0; i < pokemonList.length; i++) {
+    .then ((jsonBody) => jsonBody.results)
+    .then ((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
             const pokemon = pokemons[i]
             pokemonList.innerHTML += convertPokemonToLi(pokemon)
         }
     })
 
     .catch((error) => console.error(error))
-
-    .finally(function() {
-        console.log('Requisição finalizada!')
-    })
