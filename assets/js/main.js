240e5662d61bb1b5a2e4755656782c870eb424fd
Luciano Moura de Melo Júnior <lucianomoura.dev@gmail.com>
2026-03-25 10:15:40 -0300

    UpdadePokeApi

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,14 @@ console.log('Mão na massa!')
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon" id="pokemon">
+        <li class="pokemon ${pokemon.type}" id="pokemon">
            <div class="pokemon-header">
                 <span class="pokemon-name">${pokemon.name}</span>
-                <span class="pokemon-number">#001</span>
+                <span class="pokemon-number">#${pokemon.number}</span>
             </div>
             <div class="pokemon-info">
                 <ol class="type">
-                    <li class="race">Grass</li>
-                    <li class="attack">Poison</li>
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
                 <img src="https://www.smogon.com/dex/media/sprites/xy/${pokemon.name.toLowerCase()}.gif" alt="${pokemon.name}">
            </div>
